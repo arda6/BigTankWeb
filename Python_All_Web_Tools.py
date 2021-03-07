32adbc9974927ad5d67a85b8e049d9658d0e6d97
arda6 <74415046+arda6@users.noreply.github.com>
2021-03-07 14:08:20 +0300

    Update Python_All_Web_Tools.py

--- Python_All_Web_Tools.py
+++ Python_All_Web_Tools.py
@@ -1,90 +1,69 @@
 import os
-print(""" 
+import questionary
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
+print(Fore.GREEN + """
 
-Merhabalar. Python İle Toplu Web Araçlarına Hoşgeldin.
-Bu Program Web Araçlarını Kullanarak Toplu Bir Şekilde Taramana Yardımcı Olur.
-Coded By arda6
-Programı Kullanarak Tüm Sorumluluğu Üzerine Almış Olursun!!!
-
-""")
-print("Gerekli Yüklemeler Yapılacak. Şimdilik Linux İçin İdeal Durumda. Eğer Y (Yes) Tuşuna Basarsan Yüklemeler Yapılacak")
-installer = int(input(|Y|es / |N|o:)) 
-if installer = 'Y';
-    os.system('sudo apt-get install figlet')
-    os.system('clear')
-    os.system('apt-get install nikto')
-    os.system('Kali Linux İçin İndirmeler Bu Kadar.')
-else installer = 'N';
-    print('Çıkarılıyorunuz....')
-print("""
-
-Seçenekler ;
-    
-    1) Wordpress Scan
-    2) Normal Web Scan 
-    3) Port Scan 
-    4) Sql Injection Scan
-    5) Xss Injection Scan 
-    6) Csrf Injection Scan
-    7) Code Injection Scan
-    
-""")
-
-hedef = str(input('Target Site :'))
-secim = print("Which Scan Types :")
-if secim = '1';
-wordpress = print("""
-    
-    Wordpress Scan ;
-
-    1) Normal Scan and Users Finder:
-    2) Themes Finder:
-    
-""")
-if wordpress = '1';
-    os.system("wpscan --url" " " + hedef + )
-else wordpress = '2';
-    os.system("wpscan --url" " " + hedef + " " " enumerate u ")
-
-else secim = '2';
-    print("""
-
-    1) Uniscan Scan
-    2) Nikto Scan
+M""MMM""MMM""M          dP       M""""""""M                   dP       
+M  MMM  MMM  M          88       Mmmm  mmmM                   88       
+M  MMP  MMP  M .d8888b. 88d888b. MMMM  MMMM .d8888b. 88d888b. 88  .dP  
+M  MM'  MM' .M 88ooood8 88'  `88 MMMM  MMMM 88'  `88 88'  `88 88888"   
+M  `' . '' .MM 88.  ... 88.  .88 MMMM  MMMM 88.  .88 88    88 88  `8b. 
+M    .d  .dMMM `88888P' 88Y8888' MMMM  MMMM `88888P8 dP    dP dP   `YP 
+MMMMMMMMMMMMMM                   MMMMMMMMMM                       
 
 """)
-if normal = '1';
-    os.system("uniscan -u" + hedef + "-qweds")
-else normal = '2';
-    os.system("nikto -h " + hedef + )
-
-else secim = '3';
-    port = print ("""
-    
-    1) Nmap Version Scan
-    2) Nmap OS Scani
-    
-    
+print(Style.RESET_ALL)
+soru ="""        #####################
+          #      WebTank      #
+          #####################
+          
+          """
+target = str(input("Hedef Adres :"))
+answers = ["Nmap","SQLMap","WpScan","Uniscan" , "WafW00f"]
+cikti = questionary.select(soru,choices=answers).ask()
+if cikti == 'Nmap':
+    print(Fore.BLUE + """
+    ###############
+    #   WebTank   #
+    ###############
+        
     """)
-if port = '1';
-    os.system("nmap -sV " + hedef + " ")
-if port = '2';
-    os.system("sudo nmap -sV -O " + hedef + )
-else secim = '4';
-    sql = print("""
-    
-    1) Sqlmap Sql Test
-    2) Sqlmap Sql Attack
-    
-    """) 
-if sql = '1';
-    os.system("sqlmap -u " " " + hedef + " " "--batch")
-else sql = '2';
-    os.system("sqlmap -u " " " + hedef + " " "--batch")
-else secim = '5';
-    os.system("nikto -h " + hedef +  )
-else secim = '6';
-    os.system("uniscan -u" + hedef +)
-else secim = '7';
-    os.system("uniscan -u " + hedef +)
+    print(Style.RESET_ALL)
+    os.system("nmap -T4 -A -Pn -sV --script firewall-bypass" + target + "")
+elif cikti == 'SQLMap':
+    print(Fore.BLUE + """
+       ###############
+       #   WebTank   #
+       ############### """)
+    print(Style.RESET_ALL)
+    os.system("sqlmap -u " + target + "--random-agent --hex --tamper=space2comment --dump-all --batch")
+elif cikti == 'WpScan':
+    print(Fore.BLUE + """
+    ###############
+    #   WebTank   #
+    ###############
+
+    """)
+    print(Style.RESET_ALL)
+    api_key = str(input("Wpscan Api Key :"))
+    os.system("wpscan --url" + target +"--enumerate at , ap , u --api-token" + api_key +"")
+elif cikti == 'Uniscan':
+    print(Fore.BLUE + """
+    ###############
+    #   WebTank   #
+    ###############
+
+    """)
+    print(Style.RESET_ALL)
+    os.system("sudo uniscan -u" + target + "--qweds")
+elif cikti == 'WafW00f':
+    print(Fore.BLUE + """
+    ###############
+    #   WebTank   #
+    ###############
+
+    """)
+    print(Style.RESET_ALL)
+    os.system("sudo wafw00f " + target + "")
     
